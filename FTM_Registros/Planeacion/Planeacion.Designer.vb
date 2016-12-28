6adeb70da35f3490637ac2e9fbba418da4d04e5b
Carlos Gaytán <al02710429@tecmilenio.mx>
2016-12-28 17:52:12 -0600

    Se crea carpeta Planeación

--- FTM_Registros/Planeacion/Planeacion.Designer.vb
+++ FTM_Registros/Planeacion/Planeacion.Designer.vb
@@ -29,7 +29,7 @@ Partial Class Planeacion
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.ClientSize = New System.Drawing.Size(764, 543)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Planeacion"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
